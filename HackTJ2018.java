500f02ee1f46cdf14f7b2f1c9d40c4f2f0fdb8b4
Arravind Sudhakar <arravind.sudhakar@gmail.com>
2018-03-10 21:09:57 -0500

    Add

---
+++ HackTJ2018.java
@@ -0,0 +1,19 @@
+import javax.swing.JFrame;
+import javax.swing.*;
+import java.util.*;
+import java.io.*;
+import java.awt.*;
+import java.awt.event.*;
+
+public class HackTJ2018
+{
+   public static void main(String[] args)
+   { 
+      JFrame frame = new JFrame("Phone2Action pls give us money");
+      frame.setSize(800, 600);
+      frame.setLocation(0, 0);
+      frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+      frame.setContentPane(new Menu(frame));
+      frame.setVisible(true);
+   }
+}
