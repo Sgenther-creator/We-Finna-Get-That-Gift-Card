8eecf54e142a96b6c056a8e0ecae45d1fed6a7a5
silvarHammer <37008954+silvarHammer@users.noreply.github.com>
2018-03-10 23:14:24 -0500

    a

--- Character.java
+++ Character.java
@@ -5,7 +5,7 @@ import javax.swing.ImageIcon;
 import java.awt.event.*;
 import java.awt.image.*;
 import javax.swing.*;
-import java.awt.*
+import java.awt.*;
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.*;
@@ -15,7 +15,21 @@ import java.awt.image.BufferedImage;
 import java.awt.Color;
 public class Character 
 {
-public int approval;
-public int character;
+   public int approval;
+   public int character;
+
+   public Character(int i)
+   {
+      approval = 50;
+      character = i;
+   }
+   public void decrease()
+   {
+      approval -= 10;
+   }
+   public void increase()
+   {
+      approval += 10;
+   }
 
 }
