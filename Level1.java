7019ce597f0366594f0a230fda26d30a6a83ab84
silvarHammer <37008954+silvarHammer@users.noreply.github.com>
2018-03-10 23:21:51 -0500

    a

---
+++ Level1.java
@@ -0,0 +1,25 @@
+import java.io.*;       		 //the File class
+import java.net.*;     		 //the Scanner class
+import javax.swing.JOptionPane;
+import javax.swing.ImageIcon;
+import java.awt.event.*;
+import java.awt.image.*;
+import javax.swing.*;
+import java.awt.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.awt.image.*;
+import javax.swing.Timer;
+import java.awt.image.BufferedImage;
+import java.awt.Color;
+public class Level1 extends JPanel
+{
+   private JButton Start;
+   private JButton Exit;
+
+   public Level1()
+   {
+   
+   }
+}
