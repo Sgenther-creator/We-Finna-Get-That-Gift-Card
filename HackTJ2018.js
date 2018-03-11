30c8fca769e25de6dcbdacdf49e2fda44c265a82
Arravind Sudhakar <arravind.sudhakar@gmail.com>
2018-03-10 19:25:27 -0500

    ad

--- HackTJ2018.js
+++ HackTJ2018.js
@@ -1,3 +1,7 @@
 console.log("Hello World");
 console.log("dick weed");  
 console.log("Bring Back ToysRUs");
+var c=document.getElementById("myCanvas");
+var ctx=c.getContext("2d");
+ctx.rect(20,20,150,100);
+ctx.stroke();
